686dd04789dd5392b578cb2f49909599efc15d23
kapil903 <bikhil098@gmail.com>
2026-03-13 23:16:44 +0530

    Feature Scaling

--- DataPreprocessingTools.py
+++ DataPreprocessingTools.py
@@ -30,3 +30,11 @@ print(y)
 #Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = sc.transform(x_test[:, 3:])
+print(x_train)
+print(x_test)
