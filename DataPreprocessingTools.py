4592d8f7c02d024b575a4e2b8fb5f228ca86b144
kapil903 <bikhil098@gmail.com>
2026-03-13 23:13:00 +0530

    Encoding the independent variables

--- DataPreprocessingTools.py
+++ DataPreprocessingTools.py
@@ -13,3 +13,10 @@ imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 print(x)
+
+#Encoding the Independent Variable
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers = [('encoder', OneHotEncoder(), [0])], remainder = 'passthrough')
+x = np.array(ct.fit_transform(x))
+print(x)
