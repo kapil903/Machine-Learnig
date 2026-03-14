583698136d36376115dfbc58bd42a67e58d0830d
kapil903 <bikhil098@gmail.com>
2026-03-14 23:26:55 +0530

    Training the Multiple Linear Regression model on the Training set

--- MultipleLinearRegression.py
+++ MultipleLinearRegression.py
@@ -14,3 +14,8 @@ x = np.array(ct.fit_transform(x))
 #Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+#Training the Multiple Linear Regression model on the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
