9cd020c0cf8bedeb0d34a42d9d92fccac7cc3a49
kapil903 <bikhil098@gmail.com>
2026-03-15 23:04:02 +0530

    Training the Linear Regression model on the whole dataset

--- PolynomialLinearRegression.py
+++ PolynomialLinearRegression.py
@@ -8,3 +8,8 @@ y = dataset.iloc[:, -1].values
 
 print(x)
 print(y)
+
+#Training the Linear Regression model on the whole dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
