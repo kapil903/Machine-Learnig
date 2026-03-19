2317b7a029728a0ff5322b9737903bff6902a793
rupa-9777 <rupasriputta9777@gmail.com>
2026-03-19 22:31:24 +0530

    Printing the predicted result with linear regression

--- PolynomialLinearRegression.py
+++ PolynomialLinearRegression.py
@@ -49,7 +49,7 @@ plt.show()
 
 
 #Predicting a new result with Linear Regression
-lin_reg.predict([[6.5]])
+print(lin_reg.predict([[6.5]]))
 
 #Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
+print(lin_reg_2.predict(poly_reg.fit_transform([[6.5]])))
